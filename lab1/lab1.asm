226c6af668a452bd2a94f36b4ecb00ffdf1eb506
Varenya <varenya3@illinois.edu>
2021-08-29 23:50:40 -0500

    lab1b attempt

--- lab1/lab1.asm
+++ lab1/lab1.asm
@@ -1,2 +1,12 @@
 .ORIG x3000
-; write your code here
+
+;attempt at lab 1
+
+	AND R3, R3, #0	; clear R3
+	LD R2, ASCII_offset	; load the ascii offset into R2 (#40)
+	ADD R3, R3, R2  ; add offset to R3  
+	OUT
+	HALT	; Trap x25
+
+	ASCII_offset .FILL x30	; ASCII chart character offset
+	.END
