8b43e8d14f2fa432db31d74384402461514084ae
Varenya <varenya3@illinois.edu>
2021-09-18 03:18:31 -0500

    lab3 completed

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -3,28 +3,50 @@
 
 #define M_PI 3.14159265358979323846
 
-/*
- * main
- *  DESCRIPTION: compute a set of points of a function and print the results
- *  INPUTS: console (int, float float) -- equation related constants
- *  OUTPUTS: console (x, f(x)) -- sequence of values to be printed
- *  RETURN VALUE: 0
- *  SIDE EFFECTS: none
- */
+
 int main()
 {
-    // Declare variables
 
-    // Prompt user for input
+/*
+gcc -g -Wall -std=c99 -lm lab3.c -o lab3
+<-std=c99> = standard 99 for C. Wihtout it, certain things don't work, such as declaring a variable inside of a loop
+<-g> = lets you debug
+<-Wall> = sets all warning to on
+<-lm> = links math library
+<./lab3> = runs the output file //
+use <scanf> to read in n and both omegas
+*/
 
-    // Get user input
 
-    // Compute function output
-    /* for i from 0 to n-1
-     *     compute and print xi and f(xi)
-     *     use sin() function from math.h
-     */
+
+
+//need to pass <scanf> the address of these variables (passing in a variable stores
+//a copy of the variable in an address)
+
+//Get the input
+
+    int n;
+    float omega1, omega2;
+
+    printf("Please enter the desired values of n, ω1, and ω2 in that order: \n");
+    scanf("%d %f %f", &n, &omega1, &omega2);
+
+//negativ line inputs
+//check for proper inputs
+//uneven parens with negative results
+
+//loop from 0 to 1
+//caclutate both results
+
+    for(int i=0; i<n; i++)
+    {
+        float res1 = (M_PI*i) / n;
+        float res2 = sin(res1*omega1) + (sin(res1*omega2)) /2;
+    
+        printf("%f, %f\n", res1, res2);
+    }
+    
+
 
     return 0;
 }
-
