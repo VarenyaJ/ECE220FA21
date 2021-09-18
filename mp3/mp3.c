7e7cad8f5f1ce38a461629565d47a0842ac5f94a
Varenya <varenya3@illinois.edu>
2021-09-18 03:33:36 -0500

    mp3 completed

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,29 @@
+// Print a Row from Pascal's Triangle
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
+
   int row;
 
   printf("Enter a row index: ");
-  scanf("%d",&row);
+  scanf("%d", &row);
 
-  // Write your code here
+  unsigned long x, i, value; 
 
+  for (x = 0; x <= row; x++){
+
+        value = 1; 
+
+        for (i = 1; i <= x; i++){ 
+            
+            value = (value * (row +1 -i))/i;
+        }
+    
+        printf ("%lu ", value);
+  }
+  
+  printf("\n");
   return 0;
 }
