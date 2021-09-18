f3cc2acfc930d493d33f73386162057212de335b
Jain, Varenya <varenya3@illinois.edu>
2021-09-18 15:55:30 -0400

    Update lab3.c

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,8 +1,17 @@
+//this program implements code that computes function:
+
+//f(x)=sin(ω1x)+12sin(ω2x)
+//on the interval x∈[0,π]. You program must ask user to enter all relevant values (n, ω1, ω2) and must print a table of n pairs (xi,f(xi)) where i=0,1,…,(n−1) and 
+
+//xi=iπn. Implementation must use a loop construct a
+
+
+
 #include <stdio.h>
 #include <math.h>
 
 #define M_PI 3.14159265358979323846
-
+//define pi early on
 
 int main()
 {
@@ -28,16 +37,19 @@ use <scanf> to read in n and both omegas
     int n;
     float omega1, omega2;
 
+    //use floating point numbers for scanning in
+    
     printf("Please enter the desired values of n, ω1, and ω2 in that order: \n");
     scanf("%d %f %f", &n, &omega1, &omega2);
 
-//negativ line inputs
-//check for proper inputs
-//uneven parens with negative results
+//negative line inputs?
+//check for proper inputs?
+//uneven parens with negative results?
 
 //loop from 0 to 1
 //caclutate both results
 
+    //loop for the actual function
     for(int i=0; i<n; i++)
     {
         float res1 = (M_PI*i) / n;
