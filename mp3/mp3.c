85cfc6f145f4905be683d8f9c2c5611b1f48c9e6
Jain, Varenya <varenya3@illinois.edu>
2021-09-18 16:05:45 -0400

    Update mp3.c

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -5,13 +5,19 @@
 int main()
 {
 
+  //declare row counter
   int row;
 
   printf("Enter a row index: ");
   scanf("%d", &row);
 
+  //use unsigned long for these values since the math is wrong with integers
+  //e.g. (10 * 3) / 3 ≠ (10 / 3) * 3 --> 10 ≠ 9
+  
+  
   unsigned long x, i, value; 
 
+  // in each loop, the declared variable should be less than the one declared before/outside of the respective loop
   for (x = 0; x <= row; x++){
 
         value = 1; 
